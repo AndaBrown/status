8222465ae2da68e3444e188828fbd506ad3e2f76
Anda Brown <103917160+AndaBrown@users.noreply.github.com>
2023-02-17 12:05:11 +0800

    Update

---
+++ config.js
@@ -0,0 +1,25 @@
+window.Config = {
+
+  // 显示标题
+  SiteName: 'Aurielの状态页',
+
+  // UptimeRobot Api Keys
+  // 支持 Monitor-Specific 和 Read-Only
+  ApiKeys: [
+    'ur1797356-5759b52c25c81627e6dbf01a',
+  ],
+
+  // 日志天数
+  CountDays: 60,
+
+  // 是否显示检测站点的链接
+  ShowLink: true,
+
+  // 导航栏菜单
+  Navi: [
+    {
+      text: '返回主页',
+      url: 'https://www.auriel.top/'
+    },
+  ],
+};
